ace234251a2d8285125c4723fda8efc55fc28de1
valentin <vgallouedec@kameleoon.com>
2019-07-02 05:31:42 -0400

    Script de deploiement

--- script.sh
+++ script.sh
@@ -1,4 +1,25 @@
-echo 'Hello world ! We are here : '
+#!/bin/bash
+
+
+PATH_SITE='/home/valentin/Site'
+FILE_INDEX='index.html'
+
+echo 'Start of the deployment...'
+echo 'Current path : '
 pwd
+
+echo 'Remove previous webiste...'
+echo "Location : $PATH_SITE"
+
+rm -r $PATH_SITE
+
+echo 'Remove succeed !'
+echo 'Copy of the new website...'
+
+mkdir $PATH_SITE
+cp $FILE_INDEX "$PATH_SITE/$FILE_INDEX"
+
+echo 'Copy succeed'
+echo 'End of deployment !'
 
 exit 0
